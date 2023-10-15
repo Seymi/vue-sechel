d2edebc8ffa55b27199ead1cb786d48003b30a37
Martin Seymann <martin.seymann@chello.at>
2023-10-16 01:31:40 +0200

    componente mit template

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,8 @@
+echo off
 copy smoke.html c:\xampp\htdocs\vue
 copy schleifen.html c:\xampp\htdocs\vue
+copy JBZ.html c:\xampp\htdocs\vue
+copy JBZ_component.html c:\xampp\htdocs\vue
+
+copy component_original.html c:\xampp\htdocs\vue
+echo on
