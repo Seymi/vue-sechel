3f6fcc7b310aa8f51249403b845a8ee45d1634f8
Martin Seymann <martin.seymann@chello.at>
2023-11-06 22:29:25 +0100

    Karls message with umlaut Ã„Ã¤ÃœÃ¼Ã–Ã¶ÃŸ etc. cp1252

--- vue_school/modern_JavaScript/fetch.mjs
+++ vue_school/modern_JavaScript/fetch.mjs
@@ -13,3 +13,4 @@ fetch("https://swapi.dev/api/people/2")
 
 // You forgot to mention some basic prerequisites. You have to install and start selenium and appium to run the actor from quick-start
 äöü
+ÄÖÜ
